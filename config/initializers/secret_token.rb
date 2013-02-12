97365bda75d48598606ab4f516c98b2ae48f7e52
Harry Kalodner <harry.kalodner@gmail.com>
2013-02-11 19:20:39 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giggity3::Application.config.secret_token = '7b3640b699df9bc143de8a270a6e5d73e4ff40bdb2326c9d875c5340977f7e7e96eb7ba0707fcffcb85523bc1122409b3ebff87cc970e56ecba2595ad7dd40f7'
