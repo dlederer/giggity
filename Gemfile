761fbdb5996b6687d578f599cd15adc31859c226
Harry Kalodner <harry.kalodner@gmail.com>
2013-02-11 23:12:28 -0500

    Configured for deployment to Heroku

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
-gem 'rails', '3.2.7'
-gem 'sqlite3'
+ruby '1.9.3'
+gem 'rails', '3.2.12'
+gem "heroku"
+gem 'thin'
+gem 'pg'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Giggity3::Application.config.secret_token = '7b3640b699df9bc143de8a270a6e5d73e4ff40bdb2326c9d875c5340977f7e7e96eb7ba0707fcffcb85523bc1122409b3ebff87cc970e56ecba2595ad7dd40f7'
+Giggity3::Application.config.secret_token = 'cR0A_K&MyCIbVqt$WjXk4ZNYMz.veMs(Zm(gxow9P6ItK<z#HuwNgGl4KtPKtJVEYk,+-H5QlR>0M)pyZPx3wpOvB@hqIlI<VSPA'
