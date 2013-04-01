a9d0b8f348ed91b3dc986b4871447025059776a1
Harry Kalodner <harry.kalodner@gmail.com>
2013-04-01 15:45:26 -0400

    minor

--- config/initializers/ransack.rb
+++
@@ -1,5 +0,0 @@
-Ransack.configure do |config|
-  config.search_key = :q # Changes default search parameter key name
-                             # default is :q, this can be however overridden on
-                             # ransack search creation.
-end
