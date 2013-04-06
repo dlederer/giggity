ce9d0c733e0d238e66caef62b2a38a3a4d7b0fcd
Harry Kalodner <harry.kalodner@gmail.com>
2013-04-05 20:08:23 -0400

    minor

--- app/models/legacy/base.rb
+++
@@ -1,9 +0,0 @@
-class Legacy::Base < ActiveRecord::Base
-  self.abstract_class = true
-  self.establish_connection "legacy"
-  
-  require 'core_extensions'
-  require 'acts_as_importable'
-  
-  acts_as_importable
-end
